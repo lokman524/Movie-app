44cc4bee42871db41b1ca52a49038d046106ab8d
lokman524 <79215944+lokman524@users.noreply.github.com>
2025-06-26 16:16:25 +0800

    Add files via upload

---
+++ mobile_movie_app/app/(tabs)/saved.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Image} from 'react-native'
+import React from 'react'
+import { images } from '@/constants/images'
+
+const saved = () => {
+  return (
+    <View className='flex-1 bg-primary'>
+          <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
+    </View>
+  )
+}
+
+export default saved
